7f437a1e521add2d1530deb33037101d92b8c4f1
mattclaffey <matt.claffey@codecomputerlove.com>
2016-04-12 09:58:58 +0100

    Change: changed the way the post install script works. It doesnt need paths pre-setup.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -9,30 +9,12 @@ var path          = require('path');
 
 var TASK_NAME = 'copy-assets';
 
-// Transform function for adding paths
-function projectConfigAddPaths(config) {
-	config.paths.src[TASK_NAME] = {
-		fonts: config.dirs.src + '/fonts/',
-		media: config.dirs.src + '/media/'
-	};
-
-	config.paths.dest[TASK_NAME] = {
-		fonts: config.dirs.dest + '/fonts/',
-		media: config.dirs.dest + '/media/'
-	};
-
-	return config;
-}
-
 // Exit if NODE_ENV is development
 cartridgeUtil.exitIfDevEnvironment();
 // Make sure that the .cartridgerc file exists
 cartridgeUtil.ensureCartridgeExists();
 // Run through the project setup
 cartridgeUtil.addToRc()
-	.then(function() {
-		return cartridgeUtil.modifyProjectConfig(projectConfigAddPaths);
-	})
 	.then(function(){
 		return cartridgeUtil.addModuleConfig(path.resolve('_config', 'task.' + TASK_NAME + '.js'));
 	})
